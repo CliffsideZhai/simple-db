ea1933811c87d167e358e4feca38fb3d4fabf63c
cliffsidezhai <875174996@qq.com>
2021-05-31 07:11:52 -0700

    finish lab3 exec1

--- src/java/simpledb/optimizer/IntHistogram.java
+++ src/java/simpledb/optimizer/IntHistogram.java
@@ -3,8 +3,25 @@ package simpledb.optimizer;
 import simpledb.execution.Predicate;
 
 /** A class to represent a fixed-width histogram over a single integer-based field.
+ * 表示基于单个整数字段的固定宽度直方图的类
  */
 public class IntHistogram {
+    /**
+     * bucket的数量
+     */
+    private int numBuckets;
+
+    private int min;
+
+    private int max;
+
+    private int width;
+
+    private int lastBucketWidth;
+
+    private int[] buckets;
+
+    private int ntups;
 
     /**
      * Create a new IntHistogram.
@@ -15,7 +32,7 @@ public class IntHistogram {
      * The values that are being histogrammed will be provided one-at-a-time through the "addValue()" function.
      * 
      * Your implementation should use space and have execution time that are both
-     * constant with respect to the number of values being histogrammed.  For example, you shouldn't 
+     * constant with respect to the number of values being histogrammed.  For example, you shouldn't
      * simply store every value that you see in a sorted list.
      * 
      * @param buckets The number of buckets to split the input value into.
@@ -24,6 +41,19 @@ public class IntHistogram {
      */
     public IntHistogram(int buckets, int min, int max) {
     	// some code goes here
+        this.numBuckets = buckets;
+        this.min = min;
+        this.max = max;
+
+        this.width = (max - min +1)/numBuckets;
+
+        if (this.width == 0){
+            this.width = 1;
+        }else if ((max-min+1) % numBuckets != 0 ){
+            this.numBuckets += 1;
+        }
+        this.lastBucketWidth = max - (min +(numBuckets -1)*this.width)+1;
+        this.buckets = new int[this.numBuckets];
     }
 
     /**
@@ -32,6 +62,9 @@ public class IntHistogram {
      */
     public void addValue(int v) {
     	// some code goes here
+        int pos = (v - this.min) / this.width;
+        buckets[pos] += 1;
+        ntups += 1;
     }
 
     /**
@@ -47,7 +80,52 @@ public class IntHistogram {
     public double estimateSelectivity(Predicate.Op op, int v) {
 
     	// some code goes here
-        return -1.0;
+        /**
+         * 先把 不在范围内的条件判断出去
+         */
+        if (op == Predicate.Op.EQUALS && (v < this.min || v > this.max)) return 0.0;
+        if (op == Predicate.Op.NOT_EQUALS && (v < this.min || v > this.max)) return 1.0;
+        if ((op == Predicate.Op.GREATER_THAN && v >= this.max) || (op == Predicate.Op.LESS_THAN && v <= this.min)) return 0.0;
+        if ((op == Predicate.Op.GREATER_THAN_OR_EQ && v > this.max) || (op == Predicate.Op.LESS_THAN_OR_EQ && v < this.min)) return 0.0;
+        if ((op == Predicate.Op.GREATER_THAN && v < this.min) || (op == Predicate.Op.LESS_THAN && v > this.max)) return 1.0;
+        if ((op == Predicate.Op.GREATER_THAN_OR_EQ && v <= this.min) || (op == Predicate.Op.LESS_THAN_OR_EQ && v >= this.max)) return 1.0;
+
+        /**
+         * 剩下的结果 min <= v <= max
+         */
+        int pos = (v - this.min) / this.width;
+        int bucket_beforeone_right = this.min + pos* this.width;
+        double numQualifiedTup = 0.0;
+        int curBucketWidth = this.width;
+        if (pos == numBuckets -1){
+            curBucketWidth = this.lastBucketWidth;
+        }
+
+        if (op == Predicate.Op.EQUALS) {
+            numQualifiedTup = (1.0 / curBucketWidth) * buckets[pos];
+        } else if (op == Predicate.Op.NOT_EQUALS) {
+            numQualifiedTup = ntups - (1.0 / curBucketWidth) * buckets[pos];
+        }else if (op == Predicate.Op.GREATER_THAN || op == Predicate.Op.GREATER_THAN_OR_EQ) {
+            for (int i = pos+1; i < buckets.length; ++i) {
+                numQualifiedTup += buckets[i];
+            }
+            if (op == Predicate.Op.GREATER_THAN) {
+                numQualifiedTup += ((double) (curBucketWidth - (v - bucket_beforeone_right)) / curBucketWidth) * buckets[pos];
+            } else {
+                numQualifiedTup += ((double) (curBucketWidth - (v - bucket_beforeone_right) + 1) / curBucketWidth) * buckets[pos];
+            }
+        } else {
+            // less and lessequal
+            for (int i = 0; i < pos; ++i) {
+                numQualifiedTup += buckets[i];
+            }
+            if (op == Predicate.Op.LESS_THAN ) {
+                numQualifiedTup += ((double) (v - bucket_beforeone_right) / curBucketWidth) * buckets[pos];
+            } else {
+                numQualifiedTup += ((double) (v - bucket_beforeone_right + 1) / curBucketWidth) * buckets[pos];
+            }
+        }
+        return numQualifiedTup / ntups;
     }
     
     /**
@@ -69,6 +147,6 @@ public class IntHistogram {
      */
     public String toString() {
         // some code goes here
-        return null;
+        return buckets.toString();
     }
 }
